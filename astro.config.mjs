68cae907c1248e58497c763643068a8cc3e1eff0
zlight-GA106 <2931632176@qq.com>
2026-04-14 23:57:35 +0800

    创建了index.astro，修复了markdown渲染器，让阅读体验可用

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // 魔法指令：将默认的暗色高亮切换为亮色，完美契合 Frutiger Aero
+      theme: 'github-light',
+      // 如果你喜欢代码块没有多余的背景色填充，可以加上这句
+      wrap: true 
+    }
+  }
+});
